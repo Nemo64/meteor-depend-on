67725e94b0f89fae95888a2440e706f471a15cb6
Marco Pfeiffer <m4pfeiffer+git@gmail.com>
2014-08-24 00:02:21 +0200

    moved completly to 0.9

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Create dependencies on events and other things.",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/Nemo64/meteor-depend-on.git",
   githubUrl: "https://github.com/Nemo64/meteor-depend-on.git"
 });
