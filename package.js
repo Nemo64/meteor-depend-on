5c7e6cff06ed1af1ad627b6390cd74e3af20d4c0
Marco Pfeiffer <m4pfeiffer+git@gmail.com>
2014-08-23 23:32:27 +0200

    migrated to meteor 0.9

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
   summary: "Utilities to create dependencies on events and other things.",
   version: "1.0.1",
+  git: "https://github.com/Nemo64/meteor-depend-on.git",
   githubUrl: "https://github.com/Nemo64/meteor-depend-on.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
   api.use([
     'underscore',
     'deps',
@@ -23,7 +25,7 @@ Package.on_use(function(api) {
 });
 
 Package.on_test(function (api) {
-  api.use(['depend-on', 'tinytest', 'test-helpers', 'jquery']);
+  api.use(["nemo64:depend-on", 'tinytest', 'test-helpers', 'jquery']);
   api.add_files([]);
   api.add_files([
     'src/event-test.js',
